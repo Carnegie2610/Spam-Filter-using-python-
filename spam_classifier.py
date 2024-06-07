c8379ac9294d7d83a791438fce1117ac14b914fb
Carnegie2610 <ronsard44djoumessi@gmail.com>
2024-06-07 09:06:10 +0100

    feat : spam message filter implemented and perfectly running

--- spam_classifier.py
+++ spam_classifier.py
@@ -4,45 +4,45 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
-# quelques variables globales utiles
-PATH_TO_HAM_DIR = "D:\DEV\PYTHON_PROGRAMMING\emails\ham"
-PATH_TO_SPAM_DIR = "D:\DEV\PYTHON_PROGRAMMING\emails\spam"
+# some usefull global variables 
+PATH_TO_HAM_DIR = "/home/milkovic/Naive_bayes_classifier_using_python/naive-bayes-spam-classifier-machine-learning/emails/ham"
+PATH_TO_SPAM_DIR = "/home/milkovic/Naive_bayes_classifier_using_python/naive-bayes-spam-classifier-machine-learning/emails/spam"
 
 SPAM_TYPE = "SPAM"
-HAM_TYPE = "HAM"
+HAM_TYPE = "NON-SPAM"
 
-#les tableaux X et Y seront de la meme taille et ordonnes
-X = [] # represente l'input Data (ici les mails)
-#indique s'il s'agit d'un mail ou non
-Y = [] #les etiquettes (labels) pour le training set
+#arrays X and Y are of the same sizes
+X = [] # represents the input data set in our case the mails 
+#indacte if it is a mail or not 
+Y = [] #labels for the training set 
 
 
 def readFilesFromDirectory(path, classification):
-    os.chdir(path)
-    files_name = os.listdir(path)
+    os.chdir(path) # change the working path 
+    files_name = os.listdir(path) #list the directories in the path  
     for current_file in files_name:
-        message = extract_mail_body(current_file)
-        X.append(message)
-        Y.append(classification)
+        message = extract_mail_body(current_file) #read the content of the file
+        X.append(message)#load the different mails that is the training dataset 
+        Y.append(classification)#identificattion of the corresponding data ie if the mail is a spam or not 
        
            
-#fonction de lecture du contenu d'un fichier texte donne.
-#ici, on fait un peu de traitement pour ne prendre en compte que le "corps du mail".
-# On ignorer les en-tetes des mails
-def extract_mail_body(file_name_str):
-    inBody = False
+#function to read text in a given file 
+# Also here we take care to select exclusively the body of the mail 
+# beacause we dont need the headers of the various mails
+def extract_mail_body(file_name_str): # 
+    inBody = False # 
     lines = []
-    file_descriptor = io.open(file_name_str,'r', encoding='latin1')
+    file_descriptor = io.open(file_name_str,'r', encoding='latin1')# open the file with the encoding format latin 1 which facilitates working with special characters 
     for line in file_descriptor:
         if inBody:
             lines.append(line)
         elif line == '\n':
-            inBody = True
-        message = '\n'.join(lines)
-    file_descriptor.close()
-    return message
+            inBody = True # add the the lines to the list Lines once the body of the mail is reached 
+        message = '\n'.join(lines) # join the different lines to form a complete mail 
+    file_descriptor.close() # close the file 
+    return message # return the messsage 
 
-#appel de la fonction de chargement des mails (charger les mail normaux ensuite les SPAM)
+# here we call the function to load the mails first we load the non spam mails then the spam mails 
 readFilesFromDirectory(PATH_TO_HAM_DIR, HAM_TYPE)
 readFilesFromDirectory(PATH_TO_SPAM_DIR, SPAM_TYPE)
 
@@ -60,7 +60,7 @@ targets = training_set['Y'].values
 classifier.fit(counts, targets)
 
 
-examples = ['Free Viagra now!!!', "Hi Bob, how about a game of golf tomorrow?"]
+examples = ["Free Viagra now!!!", "Hi Bob, how about a game of golf tomorrow?","Hello can i meet you tomorrow?","5) Start Your Private Photo Album Online! http://www.adclick.ws/p.cfm?o=283&s=pk007"]
 example_counts = vectorizer.transform(examples)
 predictions = classifier.predict(example_counts)
-print predictions
+print(predictions)
